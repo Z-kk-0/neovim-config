e8d344a634ac69dd6b3eb42d578eb4fe7318980d
zer0 <max.hasselwander@wiss-edu.ch>
2025-05-12 11:58:14 +0200

    fix 3

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,24 +1,16 @@
 local mason           = require("mason")
-local mason_lspconfig = require("mason-lspconfig")
 local lspconfig       = require("lspconfig")
-local cmp_caps        = require("cmp_nvim_lsp").default_capabilities()
+local mason_lspconfig = require("mason-lspconfig")
 
 mason.setup()
 
+-- Installiert/updated Server automatisch (clangd, rust-analyzer, roslyn)
 mason_lspconfig.setup({
   ensure_installed = { "clangd", "rust_analyzer", "csharp_ls" },
-  -- API-Change: automatic_installation ist jetzt ein TABLE, nicht mehr bool
-  automatic_installation = { exclude = {} },
+  automatic_installation = true,
 })
 
--- Handler-Magie: jeder installierte Server bekommt dieselben Capabilities
-mason_lspconfig.setup_handlers({
-  function(server)
-    lspconfig[server].setup({ capabilities = cmp_caps })
-  end,
-})
-
--- Extra-Server mit eigener Config
-require("lsp.c")       -- clangd-Feinschliff
-require("lsp.rust")    -- bleibt wie gehabt
-require("lsp.csharp")  -- bleibt wie gehabt
+-- Server-spezifische Configs
+require("lsp.rust")
+require("lsp.csharp")
+require("lsp.c")      --  ⇦ neu
