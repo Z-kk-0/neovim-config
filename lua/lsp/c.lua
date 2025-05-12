aae271a5c1dd7727cb4d7f1d812cadcc5e7149f0
zer0 <max.hasselwander@wiss-edu.ch>
2025-05-12 14:28:55 +0200

    fix 4

--- lua/lsp/c.lua
+++ lua/lsp/c.lua
@@ -1,10 +1,11 @@
+-- C-/C++-LSP via clangd
 local lspconfig        = require("lspconfig")
 local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 lspconfig.clangd.setup({
   capabilities = cmp_capabilities,
-  cmd = { "clangd", "--offset-encoding=utf-16" },
+  cmd = { "clangd", "--offset-encoding=utf-16" }, -- vermeidet Off-by-2-Warnungen
   init_options = {
-    clangdFileStatus = true,  
+    clangdFileStatus = true,  -- zeigt aktuellen Datei-Status in der Status-Line an
   },
 })
--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -4,11 +4,6 @@ local mason_lspconfig = require("mason-lspconfig")
 
 mason.setup()
 
--- Installiert/updated Server automatisch (clangd, rust-analyzer, roslyn)
-mason_lspconfig.setup({
-  ensure_installed = { "clangd", "rust_analyzer", "csharp_ls" },
-  automatic_installation = true,
-})
 
 -- Server-spezifische Configs
 require("lsp.rust")
